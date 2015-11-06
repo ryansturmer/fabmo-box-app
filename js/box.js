ae585b24253dfaf0782a543c17dca81e3f7c5783
Ryan Sturmer <ryansturmer@gmail.com>
2015-11-05 23:13:49 -0500

    Working tabs, fixed bugs, dogbones.

--- js/box.js
+++ js/box.js
@@ -9,6 +9,7 @@ EPS = 0.0001;
 var Turtle2D = function() {
 	this.pos = {x:0,y:0};
 	this.history = [];
+	this._mark = false;
 }
 
 Turtle2D.prototype.reverse = function() {
@@ -23,6 +24,9 @@ Turtle2D.prototype.setPos = function(x,y) {
 Turtle2D.prototype.abs = function(x,y) {
 	if((this.pos.x != x) || (this.pos.y != y)) {
 		this.setPos(x,y);
+		if(this._mark) {
+			this.pos.mark = true;
+		}
 		this.history.push(this.pos);
 	}
 }
@@ -95,6 +99,14 @@ Turtle2D.prototype.trim = function(count) {
 	this.rtrim(count);
 }
 
+Turtle2D.prototype.mark = function() {
+	this._mark = true;
+}
+
+Turtle2D.prototype.unmark = function() {
+	this._mark = false;
+}
+
 Turtle2D.prototype.bounds = function() {
 	retval = {xmin : this.pos.x, xmax : this.pos.x, ymin : this.pos.y, ymax : this.pos.y}
 	for(i in this.history) {
@@ -108,10 +120,11 @@ Turtle2D.prototype.bounds = function() {
 }
 
 var makeBoxEdge = function(width, tabs, gender, thickness, bitDiameter) {
-	var segments = tabs*2;
+	var segments = tabs*2+1;
 	var gender = gender ? -1.0 : 1.0;
 	var segLength = width/segments;
 	var bitRadius = bitDiameter/2.0;
+	var dogBone = Math.sqrt(bitRadius*bitRadius/2.0);
 
 	if(segLength < bitDiameter) {
 		throw new Error("Cannot do this many tabs.  Pocket width (" + segLength.toFixed(3) + ") is less than the bit diameter (" + bitDiameter.toFixed(3) + ").")
@@ -122,12 +135,31 @@ var makeBoxEdge = function(width, tabs, gender, thickness, bitDiameter) {
 
 	var pol = -gender;
 	for(var i=0; i<segments+1; i++) {
+
+		console.log(pol)
+
+		// Move X
 		t.abs(segLength*i - pol*bitRadius, t.pos.y);
+
+		// Dogbone
+		if(pol > 0) {
+			t.rel(pol*dogBone,-pol*dogBone);
+			t.rel(-pol*dogBone, pol*dogBone);
+		}
+
+		// Move Y
 		t.rel(0, pol*thickness);
-		if((i%1) === 0) { pol *= -1; }
+		
+
+		// Dogbone
+		if(pol < 0) {
+			t.rel(pol*dogBone,pol*dogBone);
+			t.rel(-pol*dogBone, -pol*dogBone);
+		}
+		pol *= -1;
 	}
-	t.ltrim(1);
-	t.rtrim(1);
+	t.ltrim(3);
+	t.rtrim(3);
 	return t
 }
 
@@ -135,7 +167,13 @@ var dist = function(a, b) {
 	return Math.sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
 }
 
-var makeBoxSide = function(length, width, tabs, gender, thickness, bitDiameter) {
+var midpoint = function(a, b) {
+	return {x : a.x + ((b.x-a.x)/2.0), y : a.y + ((b.y - a.y)/2.0)}
+}
+
+var makeBoxSide = function(length, width, tabs, gender, thickness, bitDiameter, tabWidth) {
+	var dx = length-2*thickness;
+
 	edge1 = makeBoxEdge(width, tabs, gender, thickness, bitDiameter);
 	edge1.pivot(0,0,Math.PI/2.0);
 	
@@ -152,10 +190,20 @@ var makeBoxSide = function(length, width, tabs, gender, thickness, bitDiameter) 
 		box.abs(p.x,p.y);
 	}
 
+	box.rel(dx/2.0 - tabWidth/2.0 - bitDiameter/2.0, 0);
+	box.mark();
+	box.rel(tabWidth, 0);
+	box.unmark();
+
 	for(i in edge2.history) {
 		p = edge2.history[i];
 		box.abs(p.x,p.y);
 	}
+
+	box.rel(-(dx/2.0 - tabWidth/2.0 - bitDiameter/2.0), 0);
+	box.mark();
+	box.rel(-tabWidth, 0);
+	box.unmark();
 
 	start = edge1.history[0];
 	box.abs(start.x, start.y)
@@ -174,7 +222,7 @@ GCodeGenerator.prototype.setOptions = function(options) {
 	this.bitDiameter = options.bitDiameter || this.bitDiameter || 0.25;
 }
 
-var generateGCodeList = function(turtle, totalDepth, depthPerPass, feedRate, plungeRate, safeZ) {
+var generateGCodeList = function(turtle, totalDepth, depthPerPass, feedRate, plungeRate, safeZ, tabThickness) {
 	retval = [];
 	var plungeRate = plungeRate.toFixed(2);
 	var feedRate = feedRate.toFixed(2);
@@ -194,8 +242,14 @@ var generateGCodeList = function(turtle, totalDepth, depthPerPass, feedRate, plu
 	// Move to position
 	retval.push('G0 X' + turtle.history[0].x.toFixed(5) + ' Y' + turtle.history[0].y.toFixed(5));
 
+	// Spindle on
+	retval.push('M4');
+	retval.push('G4 P3.0');
+
 	var depth = Math.max(totalDepth, -depthPerPass);
 	var pass = 1;
+	var tabDepth = totalDepth + tabThickness;
+
 	while(1) {
 		// Plunge
 		retval.push('(Plunge)');
@@ -203,8 +257,21 @@ var generateGCodeList = function(turtle, totalDepth, depthPerPass, feedRate, plu
 
 		// Cut contour
 		retval.push('(Cut contour pass ' + pass + ')');
+		var cutting_tab = false;
 		for(i in turtle.history) {
 			p = turtle.history[i];
+			if(p.mark && !cutting_tab) {
+				cutting_tab = true;
+				if(depth < tabDepth) {
+					retval.push('G1 Z' + tabDepth.toFixed(5) + ' F' + plungeRate);
+				}
+			}
+			if(cutting_tab && !p.mark) {
+				cutting_tab = false;
+				if(depth < tabDepth) {
+					retval.push('G1 Z' + depth + ' F' + plungeRate);
+				}				
+			}
 			retval.push('G1 X' + p.x.toFixed(5) + ' Y' + p.y.toFixed(5) + ' F' + feedRate);
 		}
 
@@ -225,6 +292,14 @@ var generateGCodeList = function(turtle, totalDepth, depthPerPass, feedRate, plu
 
 	// Pullup
 	retval.push('G0 Z' + safeZ.toFixed(3));
+	retval.push('(Spindle off)');
+	retval.push('M8');
+	retval.push('');
+	retval.push('(Go Home)');
+	retval.push('G0 X0 Y0 Z0');
+	retval.push('');
+	retval.push('(End Program)');
+	retval.push('M30');
 	return retval;
 }
 
